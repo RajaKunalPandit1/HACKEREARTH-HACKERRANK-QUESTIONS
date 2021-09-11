fd6d638a3e2e021c55bbb00ecd692b70c33be210
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-09-11 18:22:17 +0530

    Create Insert a node at the head of a linked list.cpp
    Given a pointer to the head of a linked list, insert a new node before the head. The  value in the new node should point to  and the  value should be replaced with a given value. Return a reference to the new head of the list. The head pointer given may be null meaning that the initial list is empty.
    
    Function Description
    
    Complete the function insertNodeAtHead in the editor below.
    
    insertNodeAtHead has the following parameter(s):
    
    SinglyLinkedListNode llist: a reference to the head of a list
    data: the value to insert in the  field of the new node
    Input Format
    
    The first line contains an integer , the number of elements to be inserted at the head of the list.
    The next  lines contain an integer each, the elements to be inserted, one per function call.
    
    Constraints
    
    Sample Input
    
    5
    383
    484
    392
    975
    321
    Sample Output
    
    321
    975
    392
    484
    383
    Explanation
    
    Intially the list in NULL. After inserting 383, the list is 383 -> NULL.
    After inserting 484, the list is 484 -> 383 -> NULL.
    After inserting 392, the list is 392 -> 484 -> 383 -> NULL.
    After inserting 975, the list is 975 -> 392 -> 484 -> 383 -> NULL.
    After inserting 321, the list is 321 -> 975 -> 392 -> 484 -> 383 -> NULL.
    
    Output Status:
    
    Your Insert a node at the head of a linked list submission got 5.00 points.

---
+++ Insert a node at the head of a linked list.cpp
@@ -0,0 +1,77 @@
+Output Status:
+
+Your Insert a node at the head of a linked list submission got 5.00 points.
+  
+  
+  #include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the insertNodeAtHead function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtHead(SinglyLinkedListNode* llist, int data) {
+    
+    SinglyLinkedListNode *temp = new SinglyLinkedListNode(data);
+    if( llist == NULL){
+        return temp;
+    }
+    
+    temp->next = llist;
+    return temp;
+
+}
+
+
